f4e4f5f761c99662edf1ad334942416ded86845c
Lucas Borges Lopes <lucaslopes49441008@gmail.com>
2025-10-27 21:09:51 -0300

    Verificando funcionamento da pipeline

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Olá, Givisiez! Aplicação funcionando');
+    res.status(200).send('Pipeline CI/CD funcionando!');
 });
 
 const server = app.listen(PORT, () => {
